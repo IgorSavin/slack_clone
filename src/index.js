75d0631c7360b3baf35204bc48be196ead93836c
Igor Savin <beixiaofeng@gmail.com>
2019-02-12 00:57:21 +0200

    created few components

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import App from './components/App'
 import Login from './components/Auth/Login'
 import Register from './components/Auth/Register'
+import Spinner from './Spinner'
 import * as serviceWorker from './serviceWorker'
 import firebase from './firebase'
 
@@ -18,8 +19,8 @@ import {
 import { createStore } from 'redux'
 import { Provider, connect } from 'react-redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import rootReducer from './reducers';
-import {setUser} from './actions';
+import rootReducer from './reducers'
+import { setUser } from './actions'
 
 const store = createStore(rootReducer, composeWithDevTools())
 
@@ -28,14 +29,14 @@ class Root extends Component {
     firebase.auth().onAuthStateChanged(user => {
       if (user) {
         // console.log(user)
-        this.props.setUser(user);
+        this.props.setUser(user)
         this.props.history.push('/')
       }
     })
   }
 
   render () {
-    return (
+    return this.props.isLoading ? <Spinner /> : (
       <Switch>
         <Route exact path="/" component={App} />
         <Route path="/login" component={Login} />
@@ -45,7 +46,11 @@ class Root extends Component {
   }
 }
 
-const RootWithAuth = withRouter(connect(null, {setUser})(Root));
+const mapStateToProps = state =>({
+  isLoading:state.user.isLoading
+})
+
+const RootWithAuth = withRouter(connect(mapStateToProps, { setUser })(Root))
 
 ReactDOM.render(
   <Provider store={store}>
